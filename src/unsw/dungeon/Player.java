e44cf005dcc8ca1e99cf841cf68c8351aee372d6
z5188982 <z5188982@ad.unsw.edu.au>
2019-07-10 18:06:35 +1000

    going back

--- src/unsw/dungeon/Player.java
+++ src/unsw/dungeon/Player.java
@@ -1,16 +1,13 @@
 package unsw.dungeon;
 
-import java.util.ArrayList;
-
 /**
  * The player entity
  * @author Robert Clifton-Everest
  *
  */
-public class Player extends Entity implements Subject {
+public class Player extends Entity {
 
     private Dungeon dungeon;
-    ArrayList<Observer> listObservers = new ArrayList<Observer>();
 
     /**
      * Create a player positioned in square (x,y)
@@ -23,55 +20,22 @@ public class Player extends Entity implements Subject {
     }
 
     public void moveUp() {
-    	dungeon.findObservers();
-    	String prev = "up";
-        if (getY() > 0) {
+        if (getY() > 0)
             y().set(getY() - 1);
-        }
-        notifyObservers(prev);
     }
 
     public void moveDown() {
-    	dungeon.findObservers();
-    	String prev = "down";
-        if (getY() < dungeon.getHeight() - 1) {
+        if (getY() < dungeon.getHeight() - 1)
             y().set(getY() + 1);
-        }
-        notifyObservers(prev);
     }
 
     public void moveLeft() {
-    	dungeon.findObservers();
-    	String prev = "left";
-        if (getX() > 0) {
+        if (getX() > 0)
             x().set(getX() - 1);
-        }
-        notifyObservers(prev);
     }
 
     public void moveRight() {
-    	dungeon.findObservers();
-    	String prev = "right";
-        if (getX() < dungeon.getWidth() - 1) {
+        if (getX() < dungeon.getWidth() - 1)
             x().set(getX() + 1);
-        }
-        notifyObservers(prev);
     }
-
-	@Override
-	public void registerObserver(Observer o) {
-		if(! listObservers.contains(o)) { listObservers.add(o); }
-	}
-
-	@Override
-	public void removeObserver(Observer o) {
-		listObservers.remove(o);
-	}
-
-	@Override
-	public void notifyObservers(String prev) {
-		for(Observer obs : listObservers) {
-			obs.update(this, prev);
-		}
-	}
 }
--- src/unsw/dungeon/Wall.java
+++ src/unsw/dungeon/Wall.java
@@ -1,29 +1,9 @@
 package unsw.dungeon;
 
-public class Wall extends Entity implements Observer{
+public class Wall extends Entity {
 
     public Wall(int x, int y) {
         super(x, y);
     }
-
-	@Override
-	public void update(Subject obj, String prev) {
-		if (getX() == obj.getX() && getY() == obj.getY()) {
-			switch (prev) {
-			case "up":
-				obj.moveDown();
-				break;
-			case "down":
-				obj.moveUp();
-				break;
-			case "left":
-				obj.moveRight();
-				break;
-			case "right":
-				obj.moveLeft();
-				break;
-			}
-		}
-	}
 
 }
