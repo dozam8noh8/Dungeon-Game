590f24857beedbc1b1b53a3bb4610f8924aabb96
Owen Silver <o.silver@student.unsw.edu.au>
2019-07-20 20:58:39 +1000

    wrote some more javadoc

--- src/unsw/dungeon/objectives/AndObjectives.java
+++ src/unsw/dungeon/objectives/AndObjectives.java
@@ -2,8 +2,18 @@ package unsw.dungeon.objectives;
 
 import java.util.ArrayList;
 
-public class AndObjectives implements ObjectiveCheck { //Should change name to "And"
+/**
+ * This class represents a composite objective in which all children must be complete
+ * in order to be considered complete.
+ * @authors Waqif Alam and Owen Silver
+ *
+ */
+public class AndObjectives implements ObjectiveCheck { 
 
+	/**
+	 * Checks all children objectives, if any are incomplete it will return false,
+	 * true otherwise.
+	 */
 	@Override
 	public boolean checkComplete(ArrayList<Objective> children) {
 		System.out.println("Calling AND checkComplete");
