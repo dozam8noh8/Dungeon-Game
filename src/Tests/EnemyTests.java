85026ec9e43ee676add683588052c64037ed125c
Owen Silver <o.silver@student.unsw.edu.au>
2019-07-21 16:54:14 +1000

    final commit enemy tests

--- src/Tests/EnemyTests.java
+++ src/Tests/EnemyTests.java
@@ -17,12 +17,61 @@ class EnemyTests {
 		MazeController maze = new MazeController("maze19.json");
 		Dungeon dungeon = maze.load();
 		Enemy enemy = new Enemy(dungeon, 3, 3);
+		enemy.move(1, 0);
+		assertEquals(enemy.getX(), 4);
+		assertEquals(enemy.getY(), 3);
+		enemy.move(-1, 0);
+		assertEquals(enemy.getX(), 3);
+		assertEquals(enemy.getY(), 3);
+		enemy.move(0, -1);
+		assertEquals(enemy.getX(), 3);
+		assertEquals(enemy.getY(), 2);
+		enemy.move(0, 1);
+		assertEquals(enemy.getX(), 3);
+		assertEquals(enemy.getY(), 3);
 		
-		//move right
-		enemy.x().set(enemy.getX() + 1);
-		enemy.y().set(enemy.getY());
-		assertTrue(enemy.getX() == 4, "x coord should be 4");
-		assertTrue(enemy.getY() == 3, "y coord should be 3");
 	}
+	@Test
+	void enemy_cant_move_into_wall() throws FileNotFoundException{
+		MazeController maze = new MazeController("enemyTestsWall.json");
+		Dungeon dungeon = maze.load();
+		Enemy enemy = new Enemy(dungeon, 3, 2);
+		assertTrue(enemy.getX() == 3);
+		assertTrue(enemy.getY() == 2);
+		enemy.move(0,1); //blocked by wall
+		System.out.println(enemy.getX());
+		System.out.println(enemy.getY());
+		assertTrue(enemy.getX() == 3);
+		assertTrue(enemy.getY() == 2);
+		
+	}
+	@Test
+	void enemy_cant_move_into_boulder() throws FileNotFoundException{
+		MazeController maze = new MazeController("enemyTestsBoulder.json");
+		Dungeon dungeon = maze.load();
+		Enemy enemy = new Enemy(dungeon, 3, 2);
+		assertTrue(enemy.getX() == 3);
+		assertTrue(enemy.getY() == 2);
+		enemy.move(0,1); //blocked by boulder
+		System.out.println(enemy.getX());
+		System.out.println(enemy.getY());
+		assertTrue(enemy.getX() == 3);
+		assertTrue(enemy.getY() == 2);
+		
+	}
+	/*@Test
+	void enemy_chases_player() throws FileNotFoundException{
+		MazeController maze = new MazeController("enemyTestsPlayer.json");
+		Dungeon dungeon = maze.load();
+		Player player = dungeon.getPlayer();
+		Enemy enemy = new Enemy(dungeon, 3, 1);
+		player.moveDown();
+		player.moveDown();
+		player.moveDown();
+		assert((enemy.getX() != enemy.getX()) ||( enemy.getY() != enemy.getY()));
+		
+	}*/
+
+		
 }
 	
